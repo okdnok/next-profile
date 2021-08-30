d2afb7517f61d77ef95bb3eeca52f6482b5944f8
okdnok <87271838+okdnok@users.noreply.github.com>
2021-08-31 00:15:40 +0900

    main

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
-import Layout from '../component/layout'
-import Link from 'next/link'
+import Layout from '../components/layout'
+import useSWR from 'swr'
 
+export default function Home(){
+    const func = (...args)=> fetch(...args).then(res => res.text())
+    const { data,err } = useSWR('/data.txt',func)
+
+    return(
+        <div>
+            <Layout header="Next.js" title="Top page.">
+                <div className="alert alert-primary text-center">
+                    <h5 className="mb-4">
+                        {data}
+                    </h5>
+                </div>
+            </Layout>
+        </div>
+    )
+}
 
                     
