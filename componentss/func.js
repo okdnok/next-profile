f2f89871514ceebbc4debf84a8d35c1607d02256
okdnok <87271838+okdnok@users.noreply.github.com>
2021-08-31 11:59:24 +0900

    main

---
+++ componentss/func.js
@@ -0,0 +1,9 @@
+export default {
+    'func': {
+        'tax':{
+            'caption': '入力した金額から消費税(10%)価格を計算します．',
+            'function': '(...param)=> { return Math.floor(param[0] * 1.1)}'
+        },
+        'tax2'
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,40 @@
+import {useState} from 'react'
 import Layout from '../components/layout'
 import useSWR from 'swr'
 
 export default function Home(){
-    const func = (...args)=> fetch(...args).then(res => res.text())
-    const { data,err } = useSWR('/data.txt',func)
+    const [pref, setPref] = useState({id:0, item:'name'})
+    const [ address, setAddress ] = useState('/api/hello/'
+        + pref.id + '/' + pref.item)
+    const { data, err } = useSWR(address)
+
+    const onChange = (e)=>{
+        pref.id = e.target.value
+        setPref(pref)
+        setAddress('/api/hello/' + pref.id + '/' + pref.item)
+    }
+    const onSelect = (e)=> {
+        pref.item = e.target.value
+        setPref(pref)
+        setAddress('/api/hello/' + pref.id + '/' + pref.item)
+    }
 
     return(
         <div>
             <Layout header="Next.js" title="Top page.">
                 <div className="alert alert-primary text-center">
                     <h5 className="mb-4">
-                        {data}
+                        {JSON.stringify(data)}
                     </h5>
+                    <input type="number"
+                        className="form-control form-control-sm mb-2"
+                        onChange={onChange} />
+                    <select onChange={onSelect}
+                        className="form-control form-control-sm">
+                        <option value="name">Name</option>
+                        <option value="mail">Mail</option>
+                        <option value="age">Age</option>
+                    </select>
                 </div>
             </Layout>
         </div>
